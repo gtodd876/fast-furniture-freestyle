10abdc0feaff8b4fa2ff2f22a17dd2edca31f240
Todd Matthews <gtmatthews@gmail.com>
2025-04-05 15:49:45 -0400

    wip

--- player.gd
+++ player.gd
@@ -12,20 +12,24 @@ enum PlayerState { IDLE, RUN, JUMP, CARTWHEEL }
 @export var min_zoom_amount: float = 0.9
 @export var max_zoom_amount: float = 1.5
 @onready var ray_cast_2d: RayCast2D = $Sprite2D/RayCast2D
-
 @export var landing_acceleration: float = 200.0
 @export var air_jump_speed_reduction: float = 1500.0
 @export var jump_buffer_time: float = 0.15
+@export var max_speed: float = 290.0
+@export var friction: float = 1600.0
+@export var acceleration: float = 150.0
+@export var air_friction: float = 60.0
 var air_jump = true
 var jump_buffer: bool = false
 var player_state = PlayerState.IDLE
+var was_in_air = false
 
 func calc_gravity(v: Vector2) -> float:
 	return gravity if (v.y < 0) else fall_gravity
 	
-func do_jump() -> void:
+func do_jump(is_bouncer = false) -> void:
 	change_state(PlayerState.JUMP)
-	velocity.y = jump_speed
+	velocity.y = jump_speed * 1.25 if is_bouncer else jump_speed
 	
 func do_double_jump(delta: float) -> void:
 	if  air_jump == true and PlayerState.JUMP:
@@ -40,18 +44,30 @@ func do_double_jump(delta: float) -> void:
 						
 
 func do_cartwheel() -> void:
+	change_state(PlayerState.CARTWHEEL)
 	var tween = create_tween()
 	var full_spin: int = 360
 	if $Sprite2D.flip_h == false:
-		tween.tween_property(sprite_2d, "rotation_degrees", 0, 0.4).from(full_spin - sprite_2d.rotation_degrees)
+		tween.tween_property(sprite_2d, "rotation_degrees", 360, 0.4).from(0)
+		tween.tween_property(sprite_2d, "rotation_degrees", 0, 0.0)
 	else:
-		tween.tween_property(sprite_2d, "rotation_degrees", 0, 0.4).from(full_spin - sprite_2d.rotation_degrees)
+		tween.tween_property(sprite_2d, "rotation_degrees", -360, 0.4).from(0)
+		tween.tween_property(sprite_2d, "rotation_degrees", 0, 0.0)
+	tween.tween_callback(func():
+		if is_on_floor():
+			if abs(velocity.x) > 0:
+				change_state(PlayerState.RUN)
+			else:
+				change_state(PlayerState.IDLE)
+		else:
+			change_state(PlayerState.JUMP)
+	)
+		
 
 func _ready() -> void:
 	camera_2d.zoom = Vector2(max_zoom_amount, max_zoom_amount)
 	change_state(PlayerState.IDLE)
 	var result: Rect2 = $Sprite2D.get_rect()
-	print("player", result.size)
 	
 func change_state(new_state: PlayerState) -> void:
 	player_state = new_state
@@ -65,6 +81,7 @@ func change_state(new_state: PlayerState) -> void:
 			jump_sfx.play()
 			animation_player.play("jump_up")
 		PlayerState.CARTWHEEL:
+			print("play animationd")
 			animation_player.play("cartwheel")
 
 func get_input(delta: float) -> void:
@@ -74,12 +91,10 @@ func get_input(delta: float) -> void:
 	var jump_pressed = Input.is_action_just_pressed("jump")
 	var jump_released = Input.is_action_just_released("jump")
 	var cartwheel_pressed = Input.is_action_just_pressed("cartwheel")
-	velocity.x = 0
+	var direction = x_input
 	if right:
-		velocity.x += x_input * run_speed
 		$Sprite2D.flip_h = false
 	if left:
-		velocity.x += x_input * run_speed
 		$Sprite2D.flip_h = true
 
 	if is_on_floor():
@@ -90,42 +105,84 @@ func get_input(delta: float) -> void:
 			jump_buffer = false
 		if cartwheel_pressed:
 			do_cartwheel()
-			change_state(PlayerState.RUN)
 	else:
 		if jump_pressed:
 			jump_buffer = true
 			get_tree().create_timer(jump_buffer_time).timeout.connect(on_jump_buffer_timeout)
 			do_double_jump(delta)
-		
-	if player_state == PlayerState.IDLE and velocity.x != 0:
-		change_state(PlayerState.RUN)
-	if player_state == PlayerState.RUN and velocity.x == 0:
-		change_state(PlayerState.IDLE)
-	if player_state in [PlayerState.IDLE, PlayerState.RUN] and !is_on_floor():
-		change_state(PlayerState.JUMP)
+			
+	if player_state != PlayerState.CARTWHEEL:
+		if player_state == PlayerState.IDLE and velocity.x != 0:
+			change_state(PlayerState.RUN)
+		if player_state == PlayerState.RUN and velocity.x == 0:
+			change_state(PlayerState.IDLE)
+		if player_state in [PlayerState.IDLE, PlayerState.RUN] and !is_on_floor():
+			change_state(PlayerState.JUMP)
 	
 	if jump_released:
 		if velocity.y < 0:
 			velocity.y = jump_speed / 4.0
+		# Apply acceleration or friction based on input direction
+	if is_on_floor():
+		if direction != 0:
+			# Accelerate towards max_speed in the input direction
+			velocity.x = move_toward(velocity.x, direction * max_speed, acceleration * delta)
+		else:
+			# Apply friction to slow down when no input
+			velocity.x = move_toward(velocity.x, 0, friction * delta)
+	else:
+		# Correct version - gradually reduce velocity in air when no input
+		if direction == 0:
+			velocity.x = move_toward(velocity.x, 0, air_friction * delta)
+		# Optional: You could also allow some air control
+		else:
+			velocity.x = move_toward(velocity.x, direction * max_speed * 0.8, air_friction * delta)
 			
 func on_jump_buffer_timeout() -> void:
 	jump_buffer = false
 
 func _physics_process(delta: float) -> void:
+	var was_in_air_this_frame = !is_on_floor()
 	velocity.y += calc_gravity(velocity) * delta
 	get_input(delta)
 	if is_on_floor():
 		air_jump = true
+		
 	if player_state == PlayerState.JUMP:
 		if is_on_floor():
 			change_state(PlayerState.IDLE)
 		else: # is not on floor
 			if  velocity.y > 0:
 				$AnimationPlayer.play("jump_down")
-	var y_offset_target: float = clamp(ray_cast_2d.get_collision_point().y - global_position.y - 30, -59, 112)
-	print('ray y', ray_cast_2d.get_collision_point().y, 'global y', global_position.y, 'cam offset y', camera_2d.offset.y, 'y offset target', y_offset_target)
+	var y_offset_target: float = clamp(ray_cast_2d.get_collision_point().y - $Sprite2D.global_position.y - 30, -59, -20)
 	camera_2d.offset.y = lerp(camera_2d.offset.y, y_offset_target, 0.02)
 	var zoom_target = Vector2.ONE
 	
 	move_and_slide()
+	# Detect if we just landed this frame
+	var just_landed = was_in_air_this_frame and is_on_floor()
+	# Check if there was a collision
+	if get_slide_collision_count() > 0:		
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			var collider = collision.get_collider()
+			var normal = collision.get_normal()
+			
+			if collider.name == "Bouncers" and normal.y < 0:
+				if just_landed:
+					print("Bouncing landed from above")
+					do_jump(true)
+			elif collider.name != "Bouncers":
+				match collider.name:
+					"Collectables":
+						print("Collectables")
+					"Obstacles":
+						print("Obstacles")
+					"FloorTiles":
+						print("FloorTiles")
+					"Sliders":
+						print("Sliders")
+					_:
+						print("Unknown collision name: ", collider.name)
+					
 	camera_2d.zoom = camera_2d.zoom.lerp(zoom_target, 0.02)
--- world.gd
+++ world.gd
@@ -1,1 +1,6 @@
 extends Node2D
+@onready var player: CharacterBody2D = $Player
+
+func _process(delta: float) -> void:
+	if player.global_position.y > 1000:
+		get_tree().reload_current_scene()
